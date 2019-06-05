9e129427b743a61dc555c1b6403634c98ce152bd
Subhash Kumar Singh <subh.singh007@gmail.com>
2019-06-05 21:22:39 +0530

    enable click and enter for add data

--- budget-app/app.js
+++ budget-app/app.js
@@ -22,11 +22,21 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, uiCtrl) {
     // controller code
+    
+    // add data
+    function addData() {
+        console.log('add data');
+    }
+    
     document.querySelector('.add__btn').addEventListener('click', function() {
-       console.log('button clicked'); 
+       console.log('button clicked');
+       addData();
     });
     
     document.addEventListener('keypress', function(event) {
-        console.log(event);
+        if (event.charCode === 13) {
+            console.log('enter is pressed');
+            addData();
+        }
     });
 })(budgetController, UIController); 
